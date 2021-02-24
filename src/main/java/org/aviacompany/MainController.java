976085b7f55522ea3ba959a924ad25c8ef149e57
Ksenia <kseniatetenkina@mail.ru>
2021-02-24 18:42:48 +0300

    server starting

---
+++ src/main/java/org/aviacompany/MainController.java
@@ -0,0 +1,14 @@
+package org.aviacompany;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView home() {
+        return new ModelAndView("index");
+    }
+}
